afc6bbc23240a196d0009d5a62176e63b2966a64
willian antunes <willianmoreira@vizentec.com.br>
2021-12-03 14:07:55 -0300

    add detect and darknet class

--- src/detect.cpp
+++ src/detect.cpp
@@ -1,6 +1,20 @@
 #include "detect.h"
 
-Detect::Detect()
+Detect::Detect(const char* cfg_path, const char* weight_path)
 {
+    this->cfg_path = cfg_path;
+    this->weight_path = weight_path;
+    Init_Detection_Model();
+}
+Detect::~Detect(){
 
 }
+void Detect::Init_Detection_Model(){
+    try {
+        net = load_network_custom(const_cast<char*>(cfg_path.c_str()), const_cast<char*>(weight_path.c_str()),1, 1);
+    } catch (const std::exception& err) {
+        LOG(ERROR)<<err.what()<<std::endl;
+        net = nullptr;
+    }
+}
+
--- src/detect.h
+++ src/detect.h
@@ -2,19 +2,29 @@
 #define DETECT_H
 #include <string>
 #include <vector>
+#include <map>
+#include <opencv2/opencv.hpp>
+
+#define LOG(x) std::cerr
+
+extern "C" {
+#include <darknet.h>
+}
 
 
 class Detect
 {
 public:
-    Detect();
+    Detect(const char* cfg_path, const char* weight_path);
+    void Init_Detection_Model();
+
+    virtual ~Detect();
 private:
     std::string cfg_path;
     std::string weight_path;
-    std::string class_path;
 
-    std::vector<std::string> classes;
-
+    std::map<std::string, cv::Rect> *result;
+    network *net;
 };
 
 #endif // DETECT_H
